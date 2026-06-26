4c1b8491bb7ea79e7b125b4947915b6be84c9d7d
Omkar Bhati <omkarbhati.09@gmail.com>
2026-06-26 23:03:22 +0530

    Time: 25 ms (70.93%), Space: 42.1 MB (15.99%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Codec {
+public:
+
+    
+    string serialize(TreeNode* root) {
+        if (!root)
+            return "#,";
+
+        return to_string(root->val) + "," +
+               serialize(root->left) +
+               serialize(root->right);
+    }
+
+    TreeNode* build(string &data, int &i) {
+        if (data[i] == '#') {
+            i += 2;          
+            return nullptr;
+        }
+
+        string num = "";
+        while (data[i] != ',') {
+            num += data[i];
+            i++;
+        }
+        i++; 
+
+        TreeNode* root = new TreeNode(stoi(num));
+
+        root->left = build(data, i);
+        root->right = build(data, i);
+
+        return root;
+    }
+
+    TreeNode* deserialize(string data) {
+        int i = 0;
+        return build(data, i);
+    }
+};
